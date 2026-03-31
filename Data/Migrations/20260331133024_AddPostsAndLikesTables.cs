2dc7a1bf4df9760d87c0f0c2f88ee9fa4b8531cf
frumkinav-bit <frumkinav@bk.ru>
2026-03-31 16:31:45 +0300

    Add missing tables for feed and likes

---
+++ Data/Migrations/20260331133024_AddPostsAndLikesTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DateBoard.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostsAndLikesTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
