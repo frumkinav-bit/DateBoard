8bb0d36318fc3122e5974099153316ecf723d656
frumkinav-bit <frumkinav@bk.ru>
2026-05-07 16:45:42 +0300

    Add files via upload

---
+++ Message.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace DateBoard.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+
+        [Required]
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
